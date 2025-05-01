183343a4e1f743d3ba0267ee44fe0ae5a81110be
Sylvain Bertrand <sylv.cavalier@gmail.com>
2025-05-01 18:20:14 +0200

    Allow custom domain in Rails host config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,9 @@ Rails.application.configure do
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   # config.assets.compile = true
   # config.assets.css_compressor = nil
+  
   config.hosts << "sylvaincavalier.com"
+  config.hosts << "www.sylvaincavalier.com"
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
